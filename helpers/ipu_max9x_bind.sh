9befd181a9723598a96a7d55216a7dcbc0dc2783
florent pirou <florent.pirou@intel.com>
2026-01-29 22:35:04 +0000

    helpers: fix max9x to ipu v4l2 subdev graph binding
    Signed-off-by: florent pirou <florent.pirou@intel.com>

--- helpers/ipu_max9x_bind.sh
+++ helpers/ipu_max9x_bind.sh
@@ -57,6 +57,8 @@ while IFS= read -r line; do
 	entities+=("$line")
 done < <(media-ctl -p | grep entity | sed -e 's/.*: //;s/ (.*$//')
 
+declare -A mux_to_index=([a]=0 [b]=1 [c]=2 [d]=3)
+
 find_entity() {
 	local name=$1
 	for e in "${entities[@]}"; do
@@ -65,15 +67,19 @@ find_entity() {
 			return
 		fi
 	done
-	echo "$1 not found" >&2
+	#echo "$1 not found" >&2
 	exit 1
 }
 
 des_node() {
 	local mux=$1
 	case ${mux} in
-		a|b|c|d) echo -n "max9x a";;
-		A|B|C|D) echo -n "max9x c";;
+		a-0|b-0|c-0|d-0) echo -n "max9x a";;
+		a-1|b-1|c-1|d-1) echo -n "max9x b";;
+		a-2|b-2|c-2|d-2) echo -n "max9x c";;
+		a-3|b-3|c-3|d-3) echo -n "max9x d";;
+		a-4|b-4|c-4|d-4) echo -n "max9x e";;
+		a-5|b-5|c-5|d-5) echo -n "max9x f";;
 	esac
 }
 des_src_pad() {
@@ -82,21 +88,12 @@ des_src_pad() {
 }
 des_sink_pad() {
 	local mux=$1
-	echo -n "\"$(des_node ${mux})\":$(( ( 4 + ${mux_to_index[${mux}]} ) % 2 ))"
+	echo -n "\"$(des_node ${mux})\":$(( 4 + ${mux_to_index[${mux:0:1}]} ))"
 }
 
 ser_node() {
 	local mux=$1
-	case ${mux} in
-		a) find_entity "max9x a-0";;
-		b) find_entity "max9x b-0";;
-		c) find_entity "max9x c-0";;
-		d) find_entity "max9x d-0";;
-		A) find_entity "max9x a-2";;
-		B) find_entity "max9x b-2";;
-		C) find_entity "max9x c-2";;
-		D) find_entity "max9x d-2";;
-	esac
+	find_entity "max9x ${mux}"
 }
 ser_src_pad() {
 	local mux=$1
@@ -109,50 +106,37 @@ ser_sink_pad() {
 
 sen_node() {
 	local mux=$1
-	case ${mux} in
-		a) find_entity "i2c-${sensor} a-0";;
-		b) find_entity "i2c-${sensor} b-0";;
-		c) find_entity "i2c-${sensor} c-0";;
-		d) find_entity "i2c-${sensor} d-0";;
-		A) find_entity "i2c-${sensor} a-2";;
-		B) find_entity "i2c-${sensor} b-2";;
-		C) find_entity "i2c-${sensor} c-2";;
-		D) find_entity "i2c-${sensor} d-2";;
-	esac
+	find_entity "${sensor} ${mux}"
 }
 sen_src_pad() {
 	local mux=$1
 	echo -n "\"$(sen_node ${mux})\":0"
 }
 
-# mapping for ISX031/IMX390/AR0234 mux entity to IPU7 CSI-2 entity matching.
-declare -A media_mux_capture_link=( [a]=0 [A]=0 [b]=16 [B]=16 [c]=32)
-
-# mapping for ISX031/IMX390/AR0234 mux entity to IPU7 CSI-2 entity matching.
-declare -A media_mux_csi2_link=( [A]=0 [a]=0 [b]=1 [c]=2 [d]=3 [E]=4 [F]=5 [G]=0 [g]=0 [h]=1 [i]=2 [j]=3 [K]=4 [L]=5)
+# mapping for ISX031/IMX390/AR0234 max9x entity to IPU7|IPU6 CSI-2 entity matching.
+declare -A media_mux_capture_link=( [0]=0 [1]=16 [2]=32 [3]=48 [4]=64 [5]=80)
 
 declare -A media_mux_capture_pad=(
-	[A]=0
 	[a]=0
-	[B]=1
 	[b]=1
-	[C]=2
 	[c]=2
-	[D]=3
 	[d]=3
 )
 
-
-# all available DS5 muxes, each one represent physically connected camera.
-# muxes prefix a, b, c, d referes to max96724 aggregated link cameras
-# muxes suffix 0, 1, 2 is referes to csi2 mipi port mapping
-mux_list=${mux_param:-'a-0 b-0 c-0 a-1 b-1 c-1 a-2 b-2 c-2'}
+# all available   ISX031/IMX390/AR0234 max9x entity, each one represent physically connected camera.
+# muxes prefix a, b, c, d referes to max96724 or max9292 aggregated link cameras
+# muxes suffix 0, 1, 2, 3, 4, 5 is referes to csi2 mipi port mapping
+mux_list=${mux_param:-'a-0 b-0 c-0 d-0 a-1 b-1 c-1 d-1 a-2 b-2 c-2 d-2 a-3 b-3 c-3 d-3 a-4 b-4 c-4 d-4 a-5 b-5 c-5 d-5'}
 
 # Find media device.
 # For case with usb camera plugged in during the boot,
 # usb media controller will occupy index 0
-mdev=$(${v4l2_util} --list-devices | grep -A1 ipu6 | grep media)
+mdev=$(${v4l2_util} --list-devices | grep -A100 ipu | grep media)
 [[ -z "${mdev}" ]] && exit 0
+
+# Find IPU PCI device gen name
+cap_prefix=$(${v4l2_util} --list-devices | grep ipu | grep PCI | sed 's/^\(ipu[6|7]\).*/\1/' | tr '[:lower:]' '[:upper:]')
+[[ -z "${cap_prefix}" ]] && exit 0
 
 out() {
 	echo -n "${@}    " >&2
@@ -161,62 +145,44 @@ out() {
 }
 
 media_ctl_cmd="${media_util} -d ${mdev}"
-# media-ctl -r # <- this can be used to clean-up all bindings from media controller
+media-ctl -r # <- this can be used to clean-up all bindings from media controller
 # cache media-ctl output
 dot=$($media_ctl_cmd --print-dot)
 
-# all available IMX390/ISX031/AR0234 muxes, each one represent physically connected camera.
-# muxes prefix a, b, c, d referes to max96724 aggregated link cameras
-# muxes suffix 0, 1, 2 is referes to csi2 mipi port mapping
-
-for mux in $mux_list; do
-	e="$(sen_node ${mux})" 
+# loop over all available IMX390/ISX031/AR0234 max9x, each one represent physically connected camera.
+for camera in $mux_list; do
+	e="$(sen_node ${camera})" 
 	if [ -z "${e}" ]; then
 		continue;
 	fi
 
-	[[ $quiet -eq 0 ]] && echo "Bind MAX9X mux ${mux} .. " >&2
-
+	[[ $quiet -eq 0 ]] && echo "Bind $cap_prefix to ${sensor} ${camera} through max9x serdes .. " >&2
 
 	csi2="$((${camera:2:1}))"
 	mux=${camera:0:1}
 
-	# mapping for DS5 mux entity to IPU7 ISYS entity matching.
-	isys_cap="$((${media_mux_capture_link[${mux}]}))"
+	# mapping for MAX9x mux entity to IPU7|IPU6 ISYS matching entity.
+	cap_pad="${media_mux_capture_pad[${mux}]}"
+	isys_cap="$((${media_mux_capture_link[${csi2}]}+${cap_pad}))"
+	vc_id=$((${cap_pad} % 4))
 
-	# for DS5 aggregated mux (above a) - use ISYS capture pads with offset 6
-	# this offset used as capture sensor set - dept,rgb,ir,imu = 4
-	cap_pad=0
-	if [ "${mux}" \= "b" ]; then
-	    cap_pad=6
-	else
-	    if  [ "${mux}" \= "c" ]; then
-		metadata_enabled=0
-		cap_pad=1
-	    else
-		if  [ "${mux}" \= "d" ]; then
-		    metadata_enabled=0
-		    cap_pad=7
-		fi
-	    fi
-	fi
-	isys_cap=$((${isys_cap}+${cap_pad}))
+	out $media_ctl_cmd -l "$(des_src_pad ${camera}) -> \"Intel ${cap_prefix} CSI2 ${csi2}\":0[1]"
+	out $media_ctl_cmd -l "\"Intel ${cap_prefix} CSI2 ${csi2}\":$((${cap_pad}+1)) -> \"Intel ${cap_prefix} ISYS Capture ${isys_cap}\":0[1]"
 
-	# out $media_ctl_cmd -l "${des_node_src[${mux}]} -> \"Intel IPU6 ${csi2}\":0[1]"
-	# out $media_ctl_cmd -l "$(des_src_pad ${mux}) -> \"Intel IPU7 ISYS ${csi2}\":0[1]"
-	out $media_ctl_cmd -l "\"Intel IPU7 ${csi2}\":$((${cap_pad}+1)) -> \"Intel IPU7 ISYS ${isys_cap} $((${cap_pad}+0))\":0[5]"
+	# subdev entity '['pad-number '/' stream-number '->' pad-number '/' stream-number '[' route-flags ']' ']' ;
+	out $media_ctl_cmd -R "\"$(des_node ${camera})\"[4/0->0/0[1], 5/1->0/1[1], 6/2->0/2[1], 7/3->0/3[1]]"
+	out $media_ctl_cmd -R "\"Intel ${cap_prefix} CSI2 ${csi2}\"[0/0->1/0[1], 0/1->2/1[1], 0/2->3/2[1], 0/3->4/3[1]]"
 
-	out $media_ctl_cmd -V "$(sen_src_pad ${mux}) ${fmt}"
-	out $media_ctl_cmd -V "$(ser_sink_pad ${mux}) ${fmt}"
-	out $media_ctl_cmd -V "$(ser_src_pad ${mux}) ${fmt}"
-	out $media_ctl_cmd -V "$(des_sink_pad ${mux}) ${fmt}"
-	out $media_ctl_cmd -V "$(des_src_pad ${mux}) ${fmt}"
+	out $media_ctl_cmd -V "$(sen_src_pad ${camera}) ${fmt}"
+	out $media_ctl_cmd -V "$(ser_sink_pad ${camera}) ${fmt}"
+	out $media_ctl_cmd -V "$(ser_src_pad ${camera}) ${fmt}"
+	out $media_ctl_cmd -V "$(des_sink_pad ${camera})/${vc_id} ${fmt}"
+	out $media_ctl_cmd -V "$(des_src_pad ${camera})/${vc_id} ${fmt}"
 
-	out $media_ctl_cmd -V "\"Intel IPU7 ${csi2}\":0 ${fmt}"
-	out $media_ctl_cmd -V "\"Intel IPU7 ${csi2} $((${cap_pad}+0))\":0 ${fmt}"
-	out $media_ctl_cmd -V "\"Intel IPU7 ${isys_cap} $((${cap_pad}+0))\":0 ${fmt}"
+	out $media_ctl_cmd -V "\"Intel ${cap_prefix} CSI2 ${csi2}\":0/${vc_id} ${fmt}"
+	out $media_ctl_cmd -V "\"Intel ${cap_prefix} CSI2 ${csi2}\":$((${cap_pad}+1))/${vc_id} ${fmt}"
 
-	cap_dev=$($media_ctl_cmd -e "Intel IPU7 ISYS ${isys_cap} $((${cap_pad}+0))")
-	out ln -snf ${cap_dev} /dev/video-${sensor}-${mux}
+	cap_dev=$($media_ctl_cmd -e "Intel ${cap_prefix} ISYS Capture ${isys_cap}")
+	out ln -snf ${cap_dev} /dev/video-${sensor}-${camera}
 
 done
